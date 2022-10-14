2f60ba25e056687e6e42a8f6ef0c6a6e13e5592a
riyasshri <94245666+riyasshri@users.noreply.github.com>
2022-10-14 11:51:04 +0530

    monday update

--- frontend/nav.js
+++ frontend/nav.js
@@ -1,7 +1,63 @@
 const loadnav = async () => {
   var ele = document.getElementById("nav_inject");
-  ele.innerHTML = `
+  var stat = localStorage.getItem("user_data");
+  if (stat) {
+    ele.innerHTML = `
+    <a href="/frontend" style="float: left">
+      <img class="logo" src="../img/logo.jpg" alt="logo" height="70" width="70" />
+    </a>
+    <form id="form" onsubmit="search()">
+    <input id="query" />
+      <button>
+        <svg viewBox="0 0 1024 1024">
+          <path class="path1"
+            d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z">
+          </path>
+        </svg>
+      </button>
+    </form>
+    <div class="navbar">  
+    <a href="" onclick="logout()">
+        <button>Logout</button>
+      </a>  
+    <a href="/frontend/profile/profile.html">
+        <button>Profile</button>
+      </a>
+      <a href="/frontend/add/add.html">
+        <button href="">Add item</button>
+      </a>
+    </div>
   `
+  }
+  else {
+    ele.innerHTML = `
+    <a href="/frontend" style="float: left">
+      <img class="logo" src="../img/logo.jpg" alt="logo" height="70" width="70" />
+    </a>
+    <form id="form" onsubmit="search()">
+    <input id="query" />
+      <button>
+        <svg viewBox="0 0 1024 1024">
+          <path class="path1"
+            d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z">
+          </path>
+        </svg>
+      </button>
+    </form>
+    <div class="navbar">
+      <a href="login/login.html">
+        <button>Login</button>
+      </a>
+      <a href="signup/signup.html">
+        <button href="">Sign up</button>
+      </a>
+      <a href="about/about.html">
+        <button href="">About</button>
+      </a>
+    </div>
+  `
+  }
+
 }
 
 const logout = async () => {
@@ -13,6 +69,17 @@ const logout = async () => {
   }
 };
 
-const search=async()=>{
-  
+const search = async () => {
+  var search_query = document.getElementById("query").value;
+  location.href = `http://localhost:5500/frontend/result/result.html?search_query=${search_query}`;
 }
+
+const verify = async () => {
+  var user_data = localStorage.getItem("user_data");
+  if (!user_data) {
+    location.href = `http://localhost:5500/frontend/login/login.html`;
+  }
+  return;
+};
+
+
